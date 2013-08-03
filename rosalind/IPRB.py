e065a64a0fb14d5791417ec41a476f885817814a
Paul Inder <paulinder@gmail.com>
2013-08-03 13:37:29 -0400

    worked on iprb

--- rosalind/IPRB.py
+++ rosalind/IPRB.py
@@ -6,15 +6,13 @@
 from sys import argv
 
 def possesses_dominant_allele(k,m,n):
-	tp = k + m + n	#	total pop
+	tp = float(k + m + n)	#	total pop
 	#	probabilities of passing on dominant allele:
-	pk = 1.0
-	pm = 0.5
-	pn = 0.0
+	pk = 1.0;   pm = 0.5;   pn = 0.0
 	
 	prob = 0.0
 	
-	prob = prob + pk*pk*(k/tp)*((k-1)/(tp-1))	# k mate with k
+	prob = prob + pk*pk*(k/tp)*((k-1)/(tp-1))       # k mate with k
 	prob = prob + pk*pm*(k/tp)*((m)/(tp-1))			# k mate with m
 	prob = prob + pk*pn*(k/tp)*((n)/(tp-1))			# k mate with n
 	prob = prob + pm*pm*(m/tp)*((m-1)/(tp-1))		# m mate with m
