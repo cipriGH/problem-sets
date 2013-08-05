911b05c534eac17128eeed4c936343dde6adf86d
Paul Inder <paulinder@gmail.com>
2013-08-04 22:09:10 -0400

    solved fibd

---
+++ rosalind/FIBD.py
@@ -0,0 +1,31 @@
+"""
+	FIBD:	Mortal Fibonacci Rabbits
+"""
+from sys import argv
+
+def rabbit_population(n,m=1):
+	
+	ages = [1] + [0]*(m-1)
+	for i in xrange(n-1):
+		ages = [sum(ages[1:])] + ages[:-1]
+	return sum(ages)
+		
+
+def test_FIBD():
+	n=6
+	m=3
+	answer = 4
+	
+	rp = rabbit_population(n,m)
+	print rp
+	assert(answer==rp)
+
+def main():
+	n,m = argv[1:3]
+	n = int(n)
+	m = int(m)
+
+	print rabbit_population(n,m)
+
+if __name__=='__main__':
+	main()
